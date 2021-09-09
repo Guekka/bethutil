6d77384ee593eb34e65d4db669a908d8e8e52ef0
G'k <39066502+Guekka@users.noreply.github.com>
2021-09-09 15:25:00 +0200

    [bsa] Make pack() multithreaded

--- src/pack.cpp
+++ src/pack.cpp
@@ -31,7 +31,7 @@ void write(bool compressed, ArchiveData &&data, const Settings &sets, const Path
     compressed &= data.get_type() != ArchiveType::Incompressible;
 
     auto arch = Archive(data.get_version(), compressed);
-    std::for_each(data.begin(), data.end(), [&](auto &&f) { arch.add_file(root, f); });
+    std::for_each(std::execution::par, data.begin(), data.end(), [&](auto &&f) { arch.add_file(root, f); });
 
     arch.write(data.find_name(root, sets));
 }
