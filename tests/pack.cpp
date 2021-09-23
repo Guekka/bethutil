53aae4a2ef1f18091b57e0a222e02997d46d92b8
G'k <39066502+Guekka@users.noreply.github.com>
2021-09-23 16:55:34 +0200

    [bsa] Update internal test

--- tests/pack.cpp
+++ tests/pack.cpp
@@ -8,7 +8,12 @@
 #include "btu/bsa/transform.hpp"
 #include "btu/bsa/unpack.hpp"
 
+#include <catch2/catch.hpp>
+
 #include <chrono>
+#include <iostream>
+
+using namespace btu::bsa;
 
 // Intended for library devs only
 
@@ -24,11 +29,13 @@
  * Requiem MT Delphi : 6236ms
  * Unglau MT Delphi : 1494s
 */
-
+#define BETHUTIL_BSA_INTERNAL_TEST
 #ifdef BETHUTIL_BSA_INTERNAL_TEST
-//const auto dir = R"(F:\Edgar\Downloads\unglaubliche Reise EXTENDED Version SSE DV 2.0 BSA)";
 const auto dir
-    = R"(E:\Programmes\Mod_Skyrim_SE\Cathedral Assets Optimizer\TESTS\TES5_TO_SSE\BSACreation\INPUT - Copy)";
+    // = R"(E:\Programmes\Mod_Skyrim_SE\Cathedral Assets Optimizer\TESTS\TES5_TO_SSE\BSACreation\INPUT - Copy)";
+    //= R"(F:\Edgar\Downloads\mods\unglaubliche Reise EXTENDED Version SSE DV 2.0 BSA)";
+    // = R"(C:\Data\BSA)";
+    = R"(C:\Skyrim\Chanterelle\Data - Copy)";
 
 template<typename Func>
 void run(Func const &f, std::string const &name)
@@ -49,14 +56,50 @@ void run(Func const &f, std::string const &name)
     std::cout << name << " took " << time << "ms" << std::endl;
 }
 
+std::string time()
+{
+    auto now       = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << "[" << std::put_time(std::localtime(&in_time_t), "%H %M %S") << "] ";
+    return ss.str();
+}
+
 TEST_CASE("Create BSA")
 {
     auto sets = Settings::get(Game::SSE);
+    run([&] { split(dir, sets); }, "Split");
     //cleanDummyPlugins(dir, sets);
-    makeDummyPlugins(dir, sets);
-    run([] { create(dir, true, Settings::get(Game::SSE)); }, "create");
-    run([] { extractAll(dir, Settings::get(Game::SSE)); }, "extract");
+    //makeDummyPlugins(dir, sets);
     run(
+        [&sets] {
+            std::cout << time() << "start split";
+            auto bsas = split(dir, sets);
+            merge(bsas);
+            for (auto bsa : std::move(bsas))
+            {
+                std::cout << time() << bsa.find_name(dir, sets) << std::endl;
+                write(true, std::move(bsa), sets, dir);
+            }
+        },
+        "create");
+
+    run(
+        [&sets] {
+            std::vector files(fs::directory_iterator(dir), fs::directory_iterator{});
+            erase_if(files, [&sets](const auto &file) { return file.path().extension() != sets.extension; });
+            const auto out = dir / Path("tmp");
+            std::for_each(files.begin(), files.end(), [&out](const auto &file) {
+                std::cout << time() << file.path().generic_string() << std::endl;
+                btu::bsa::unpack(UnpackSettings{.file_path                = file.path(),
+                                                .overwrite_existing_files = true,
+                                                .root_opt                 = &out});
+            });
+        },
+        "extract");
+
+    /*run(
         [] {
             libbsarch::bsa bsa;
             transform(
@@ -65,6 +108,6 @@ TEST_CASE("Create BSA")
                 [](auto, auto data) { return libbsarch::to_vector(std::move(data)); },
                 Settings::get(Game::SSE));
         },
-        "transform");
+        "transform");*/
 }
 #endif
