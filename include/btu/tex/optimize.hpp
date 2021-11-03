ad2e5e1bc6de23bf090ee7674c62509c1ce446c3
G'k <39066502+Guekka@users.noreply.github.com>
2021-11-03 13:37:26 +0100

    [tex] WIP: Add declaration of optimize function
    Definition is wip

---
+++ include/btu/tex/optimize.hpp
@@ -0,0 +1,17 @@
+/* Copyright (C) 2021 Edgar B
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "btu/tex/functions.hpp"
+
+namespace btu::tex {
+
+struct Settings
+{
+};
+
+[[nodiscard]] auto optimize(const ScratchImage &tex, Settings sets) noexcept -> Result;
+} // namespace btu::tex
---
+++ src/optimize.cpp
@@ -0,0 +1,10 @@
+/* Copyright (C) 2021 Edgar B
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "btu/tex/optimize.hpp"
+
+#include <DirectXTex.h>
+
+btu::tex::Result btu::tex::optimize(const ScratchImage &tex, Settings sets) noexcept {}
