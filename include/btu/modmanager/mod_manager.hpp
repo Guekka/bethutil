79e3482b004e44f6b220d7ca978e3a5c52b605f9
G'k <39066502+Guekka@users.noreply.github.com>
2022-01-15 09:18:18 +0100

    [modmanager] Add find_manager

--- include/btu/modmanager/mod_manager.hpp
+++ include/btu/modmanager/mod_manager.hpp
@@ -1,73 +1,21 @@
-/* Copyright (C) 2020 G'k
+/* Copyright (C) 2020 - 2022 Edgar B
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 #pragma once
 
-#include "BaseTypes.hpp"
-#include "Settings.hpp"
-#include "Utils/QJSONValueWrapper.hpp"
+#include "btu/common/path.hpp"
 
-#include <QCoreApplication>
+namespace btu::modmanager {
+[[maybe_unused]] constexpr auto k_force_process_folder = "ForceProcess.cao";
 
-#define REGISTER_SETTING(type, name, key) \
-    QJSONValueWrapper<type> name{json_, nlohmann::json_pointer<nlohmann::json>{key}};
-
-class QDir;
-
-namespace CAO {
-class GeneralSettings final : public Settings
+enum class ModManager
 {
-    Q_DECLARE_TR_FUNCTIONS(GeneralSettings)
-public:
-    GeneralSettings() = default;
-    GeneralSettings(nlohmann::json j);
-    GeneralSettings(const GeneralSettings &other);
-    GeneralSettings(GeneralSettings &&other) noexcept;
-
-    GeneralSettings &operator=(const GeneralSettings &other);
-    GeneralSettings &operator=(GeneralSettings &&other) noexcept;
-
-    std::optional<QString> isValid() const override;
-
-private:
-    static inline const QString forceProcessFolder = "ForceProcess.cao";
-
-    enum class ModManager
-    {
-        Vortex,
-        MO2,
-        Kortex,
-        ManualForced,
-        None
-    };
-    ModManager findManager(const QDir &dir) const;
-
-public:
-    REGISTER_SETTING(bool, bBSACreate, "/BSA/bBSACreate")
-    REGISTER_SETTING(bool, bBSAExtract, "/BSA/bBSAExtract")
-    REGISTER_SETTING(bool, bBSAProcessContent, "/BSA/bBSAProcessContent")
-
-    REGISTER_SETTING(bool, bBSADontMakeLoaded, "/BSA/bBSADontMakeLoaded")
-    REGISTER_SETTING(bool, bBSADontCompress, "/BSA/bBSADontCompress")
-    REGISTER_SETTING(bool, bBSADontMergeTextures, "/BSA/bBSADontMergeTextures")
-    REGISTER_SETTING(bool, bBSADontMergeIncomp, "/BSA/bBSADontMergeIncomp")
-
-    REGISTER_SETTING(double, iBSAMaxSize, "/Advanced/BSA/iBSAMaxSize")
-    REGISTER_SETTING(bool, bBSATexturesEnabled, "/Advanced/BSA/bBSATexturesEnabled")
-    REGISTER_SETTING(double, iBSATexturesMaxSize, "/Advanced/BSA/iBSATexturesMaxSize")
-
-    REGISTER_SETTING(bool, isBaseProfile, "/General/isBaseProfile")
-
-    REGISTER_SETTING(bool, bDryRun, "/General/bDryRun")
-    REGISTER_SETTING(OptimizationMode, eMode, "/General/eMode")
-    REGISTER_SETTING(Games, eGame, "/General/Game")
-    REGISTER_SETTING(QString, sCurrentPattern, "/General/sCurrentPattern")
-
-    REGISTER_SETTING(bool, bEnableOutputPath, "/General/bEnableOutputPath")
-    REGISTER_SETTING(QString, sInputPath, "/General/sInputPath")
-    REGISTER_SETTING(QString, sOutputPath, "/General/sOutputPath")
+    Vortex,
+    MO2,
+    Kortex,
+    ManualForced,
+    None
 };
-} // namespace CAO
-
-#undef REGISTER_SETTING
+auto find_manager(const common::Path &dir) -> ModManager;
+} // namespace btu::modmanager
--- src/modmanager/mod_manager.cpp
+++ src/modmanager/mod_manager.cpp
@@ -1,58 +1,17 @@
-/* Copyright (C) 2020 G'k
+/* Copyright (C) 2020 - 2022 Edgar B
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "GeneralSettings.hpp"
+#include "btu/modmanager/mod_manager.hpp"
 
-#include <QDir>
+#define _SILENCE_CLANG_COROUTINE_MESSAGE
+#include <flow.hpp>
 
-namespace CAO {
-GeneralSettings::GeneralSettings(nlohmann::json j)
-{
-    setJSON(std::move(j));
-}
-
-GeneralSettings::GeneralSettings(const GeneralSettings &other)
-{
-    json_ = other.json_;
-}
-
-GeneralSettings::GeneralSettings(GeneralSettings &&other) noexcept
-{
-    json_ = std::move(other.json_);
-}
-
-GeneralSettings &GeneralSettings::operator=(const GeneralSettings &other)
-{
-    if (this != &other)
-        json_ = other.json_;
-
-    return *this;
-}
-
-GeneralSettings &GeneralSettings::operator=(GeneralSettings &&other) noexcept
-{
-    if (this != &other)
-        json_ = std::move(other.json_);
-
-    return *this;
-}
-
+/*
 std::optional<QString> GeneralSettings::isValid() const
 {
-    if (iBSAMaxSize() < 0.5 || iBSATexturesMaxSize() < 0.5)
-        return tr("BSA Max size cannot be smaller than 0.5Gb");
-
-    if (sInputPath().size() < 5)
-        return tr("This path is shorter than 5 characters: Path: '%1'").arg(sInputPath());
-
-    const QDir inputDir(sInputPath());
-
-    if (!inputDir.exists() || sInputPath().size() < 5)
-        return tr("Input path does not exist. Path: '%1'").arg(sInputPath());
-
-    const ModManager managedBy = findManager(inputDir);
+      const ModManager managedBy = findManager(inputDir);
 
     if (eMode() == SeveralMods && managedBy == ModManager::None)
     {
@@ -70,39 +29,33 @@ std::optional<QString> GeneralSettings::isValid() const
                   "Path: '%1'")
             .arg(sInputPath());
     }
-
-    if (bEnableOutputPath())
-    {
-        const QDir outputDir(sOutputPath());
-
-        if (!outputDir.exists() || sOutputPath().size() < 5)
-            return tr("Output path does not exist. Path: '%1'").arg(sOutputPath());
-    }
-
-    return std::nullopt;
 }
+*/
 
-GeneralSettings::ModManager GeneralSettings::findManager(const QDir &dir) const
+namespace btu::modmanager {
+auto find_manager(const common::Path &dir) -> ModManager
 {
+    namespace fs = std::filesystem;
+
     /* Manual forced */
-    if (dir.exists(forceProcessFolder))
+    if (fs::exists(dir / k_force_process_folder))
         return ModManager::ManualForced;
 
     /* Vortex */
-    if (dir.exists("__vortex_staging_folder"))
+    if (fs::exists("__vortex_staging_folder"))
         return ModManager::Vortex;
 
     /* MO2 */
     //Checking 10 dirs should be enough. One of them should be enough actually, but...better be safe
-    auto first10Dirs = QDir(dir).entryList(QDir::NoDotAndDotDot | QDir::Dirs) | rx::take(10)
-                       | rx::to_vector();
-
-    for (const QString &dirName : first10Dirs)
-        if (QDir(dirName).exists("meta.ini"))
-            return ModManager::MO2;
+    const bool mo2 = flow::from(fs::directory_iterator(dir))
+                         .filter([](auto &&entry) { return entry.is_directory(); })
+                         .take(10)
+                         .any([](auto &&dir) { return fs::exists(dir.path() / "meta.ini"); });
+    if (mo2)
+        return ModManager::MO2;
 
     //Kortex not yet handled
 
     return ModManager::None;
 }
-} // namespace CAO
+} // namespace btu::modmanager
