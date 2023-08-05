f1995b1d1103461bc62e055485b8f938cd12bdd1
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-08-05 19:25:57 +0200

    refactor(bsa): simplify API by using a generator

--- include/btu/bsa/pack.hpp
+++ include/btu/bsa/pack.hpp
@@ -5,28 +5,23 @@
 #pragma once
 
 #include <btu/bsa/settings.hpp>
+#include <flux.hpp>
 
 #include <functional>
 
 namespace btu::bsa {
-using AllowFilePred  = std::function<bool(const Path &dir, fs::directory_entry const &file_info)>;
-using ArchiveNameGen = std::function<std::u8string(ArchiveType)>;
+using AllowFilePred = std::function<bool(const Path &dir, fs::directory_entry const &file_info)>;
 
 struct PackSettings
 {
     Path input_dir;
-    Path output_dir;
-
     Settings game_settings;
 
     Compression compress = Compression::Yes;
 
-    ArchiveNameGen archive_name_gen;
     std::optional<AllowFilePred> allow_file_pred = std::nullopt;
 };
 
-/// \return the list of files which failed to pack
-[[nodiscard]] auto pack(const PackSettings &settings) noexcept
-    -> std::vector<std::pair<Path, std::exception_ptr>>;
+[[nodiscard]] auto pack(PackSettings settings) noexcept -> flux::generator<bsa::Archive &&>;
 
 } // namespace btu::bsa
