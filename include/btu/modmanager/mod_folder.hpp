4e295472623f83040f69b8f6ddaa4d0b12bbf5b6
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-06-30 10:58:57 +0100

    feat(modfolder): complete refactor with better error handling and improved ergonomics

--- include/btu/modmanager/mod_folder.hpp
+++ include/btu/modmanager/mod_folder.hpp
@@ -13,24 +13,17 @@
 #include <tl/expected.hpp>
 
 namespace btu::modmanager {
-class ModFolder
+
+struct ModFile
+{
+    Path relative_path;
+    common::Lazy<tl::expected<std::vector<std::byte>, common::Error>> content;
+};
+
+class ModFolderIteratorBase
 {
 public:
-    struct ModFile
-    {
-        Path relative_path;
-        common::Lazy<tl::expected<std::vector<std::byte>, common::Error>> content;
-    };
-
-    explicit ModFolder(Path directory, bsa::Settings bsa_settings);
-
-    using Transformer = std::function<std::optional<std::vector<std::byte>>(ModFile)>;
-
-    enum class ArchiveTooLargeState : std::uint8_t
-    {
-        BeforeProcessing,
-        AfterProcessing,
-    };
+    virtual ~ModFolderIteratorBase() = default;
 
     enum class ArchiveTooLargeAction : std::uint8_t
     {
@@ -38,8 +31,55 @@ public:
         Process,
     };
 
-    using ArchiveTooLargeHandler
-        = std::function<ArchiveTooLargeAction(const Path &archive_path, ArchiveTooLargeState state)>;
+    enum class ArchiveTooLargeState : std::uint8_t
+    {
+        BeforeProcessing,
+        AfterProcessing,
+    };
+
+    [[nodiscard]] virtual auto archive_too_large(
+        const Path &archive_path, ArchiveTooLargeState state) noexcept -> ArchiveTooLargeAction = 0;
+
+    virtual void failed_to_read_archive(const Path &archive_path) noexcept {}
+};
+
+class ModFolderTransformer : public ModFolderIteratorBase
+{
+public:
+    /// \brief Takes a file content and returns a new content. If the function returns
+    /// std::nullopt, the file is not changed.
+    /// \note For optimal performance, it is recommended to return std::nullopt for files that do not need to be changed.
+    [[nodiscard]] virtual auto transform_file(ModFile file) noexcept -> std::optional<std::vector<std::byte>>
+                                                                        = 0;
+
+    virtual void failed_to_write_transformed_file(const Path &relative_path,
+                                                  std::span<const std::byte> content) noexcept
+    {
+    }
+
+    virtual void failed_to_read_transformed_file(const Path &relative_path,
+                                                 std::span<const std::byte> content) noexcept
+    {
+    }
+
+    virtual void failed_to_write_archive(const Path &old_archive_path, const Path &new_archive_path) noexcept
+    {
+    }
+};
+
+class ModFolderIterator : public ModFolderIteratorBase
+{
+public:
+    virtual void process_file(ModFile file) noexcept = 0;
+};
+
+class ModFolder
+{
+public:
+    using enum ModFolderIteratorBase::ArchiveTooLargeAction;
+    using enum ModFolderIteratorBase::ArchiveTooLargeState;
+
+    explicit ModFolder(Path directory, bsa::Settings bsa_settings);
 
     /// Get the size of the folder, including files in archives.
     /// Utility function, equivalent to iterate() and counting the files.
@@ -47,22 +87,16 @@ public:
 
     /// Transform all files in the folder, including files in archives.
     /// Multithreaded.
-    /// \arg transformer Function that takes a file content and returns a new content. If the function returns
-    /// std::nullopt, the file is not changed.
-    /// \note For optimal performance, it is recommended to return std::nullopt for files that do not need to be changed.
-    void transform(const Transformer &transformer,
-                   ArchiveTooLargeHandler &&archive_too_large_handler) noexcept;
+    void transform(ModFolderTransformer &transformer) noexcept;
 
     /// Iterate over all files in the folder, including files in archives.
     /// Multithreaded.
-    void iterate(const std::function<void(ModFile)> &visitor,
-                 ArchiveTooLargeHandler &&archive_too_large_handler) const noexcept;
+    void iterate(ModFolderIterator &transformer) const noexcept;
 
     /// Iterate over all files in the folder, including archives.
     /// Multithreaded.
-    void iterate(
-        const std::function<void(Path relative_path)> &loose,
-        const std::function<void(const Path &archive_path, bsa::Archive &&archive)> &archive) const noexcept;
+    void iterate(const std::function<void(Path relative_path)> &loose,
+                 const std::function<void(const Path &archive_path)> &archive) const noexcept;
 
     [[nodiscard]] auto name() const noexcept -> std::u8string { return dir_.filename().u8string(); }
     [[nodiscard]] auto path() const noexcept -> const Path & { return dir_; }
@@ -71,8 +105,7 @@ private:
     // This function is used to implement transform() and one of the iterate() functions.
     // The reason for it to be private is that while it is `const`, it is not `const` semantically: it modifies the
     // folder
-    void transform_impl(const Transformer &transformer,
-                        ArchiveTooLargeHandler &&archive_too_large_handler) const noexcept;
+    void transform_impl(ModFolderTransformer &transformer) const noexcept;
 
     Path dir_;
     bsa::Settings bsa_settings_;
