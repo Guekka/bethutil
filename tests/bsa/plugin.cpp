bd4cd81136e49a19ef876964dc4ec189ed048191
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-06-30 12:07:04 +0200

    tests(bsa): improve test coverage

--- tests/bsa/plugin.cpp
+++ tests/bsa/plugin.cpp
@@ -14,9 +14,9 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
     {
         auto sets = Settings::get(btu::Game::SSE);
 
-        REQUIRE_FALSE(FilePath::make("", sets, FileTypes::Plugin).has_value());
+        CHECK_FALSE(FilePath::make("", sets, FileTypes::Plugin).has_value());
         auto plug = FilePath::make("C:/SomeDir/Requiem.esp", sets, FileTypes::Plugin);
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->dir == "C:/SomeDir");
         CHECK(plug->name == u8"Requiem");
         CHECK(plug->suffix.empty());
@@ -28,7 +28,7 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
 
         auto plug  = FilePath::make("C:/SomeDir/Requiem - Textures01.bsa", sets, FileTypes::BSA);
         auto plug2 = FilePath::make("C:/SomeDir/Requiem01 - Textures.bsa", sets, FileTypes::BSA);
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->counter == plug2->counter); // Digits can be after or before suffix
         CHECK(plug->dir == "C:/SomeDir");
         CHECK(plug->name == u8"Requiem");
@@ -43,7 +43,7 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
                                    sets,
                                    FileTypes::BSA);
 
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->dir == "C:/AnotherSomeDir");
         CHECK(plug->name == u8"Requiem01 - Enhancement");
         CHECK(plug->suffix == u8"Textures");
@@ -55,7 +55,7 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
         auto sets = Settings::get(btu::Game::SSE);
         auto plug = FilePath::make("C:/AnotherSomeDir/Requiem - Enhancement01.bsa", sets, FileTypes::BSA);
 
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->dir == "C:/AnotherSomeDir");
         CHECK(plug->name == u8"Requiem - Enhancement");
         CHECK(plug->suffix.empty());
@@ -67,7 +67,7 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
         auto sets = Settings::get(btu::Game::FO4);
         auto plug = FilePath::make("C:/Mk. II - Frag.esp", sets, FileTypes::Plugin);
 
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->name == u8"Mk. II - Frag");
         CHECK(plug->suffix.empty());
         CHECK_FALSE(plug->counter.has_value());
@@ -78,7 +78,7 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
         auto sets = Settings::get(btu::Game::FO4);
         auto plug = FilePath::make("some_dir/Colt 6520.esp", sets, FileTypes::Plugin);
 
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->name == u8"Colt ");
         CHECK(plug->suffix.empty());
         CHECK(plug->counter == 6520U);
@@ -89,7 +89,7 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
         auto sets = Settings::get(btu::Game::FO4);
         auto plug = FilePath::make("some_dir/Colt. 6520.esp", sets, FileTypes::Plugin);
 
-        REQUIRE(plug.has_value());
+        CHECK(plug.has_value());
         CHECK(plug->name == u8"Colt. ");
         CHECK(plug->suffix.empty());
         CHECK(plug->counter == 6520U);
@@ -97,19 +97,117 @@ TEST_CASE("Plugin names are correctly parsed", "[src]")
     }
 }
 
+TEST_CASE("FilePath::make edge cases")
+{
+    auto sets = Settings::get(btu::Game::SSE);
+
+    SECTION("FilePath::make handles invalid paths", "[src]")
+    {
+        auto plug = FilePath::make("", sets, FileTypes::Plugin);
+        CHECK_FALSE(plug.has_value());
+    }
+
+    SECTION("FilePath::make handles paths without file extension", "[src]")
+    {
+        auto plug = FilePath::make("C:/SomeDir/Requiem", sets, FileTypes::Plugin);
+        CHECK_FALSE(plug.has_value());
+    }
+
+    SECTION("FilePath::make handles paths with unsupported file extension", "[src]")
+    {
+        auto plug = FilePath::make("C:/SomeDir/Requiem.txt", sets, FileTypes::Plugin);
+        CHECK_FALSE(plug.has_value());
+    }
+}
+
 TEST_CASE("clean_dummy_plugins", "[src]")
 {
-    const Path dir = "bsa_clean_dummy_plugins";
-    const Path out = dir / "out";
-
-    btu::fs::remove_all(out);
-    btu::fs::copy(dir / "in", out);
-
     const auto &sets = Settings::get(btu::Game::SSE);
 
-    auto plugins = list_plugins(btu::fs::directory_iterator(out), btu::fs::directory_iterator(), sets);
-    REQUIRE(plugins == std::vector{FilePath(out, u8"dummy_sse", u8"", u8".esp", FileTypes::Plugin)});
-    clean_dummy_plugins(plugins, sets);
-    REQUIRE(plugins.empty());
-    REQUIRE(btu::common::compare_directories(out, dir / "expected"));
+    SECTION("base case")
+    {
+        const Path dir = "bsa_clean_dummy_plugins";
+        const Path out = dir / "out";
+
+        btu::fs::remove_all(out);
+        btu::fs::copy(dir / "in", out);
+
+        auto plugins = list_plugins(btu::fs::directory_iterator(out), btu::fs::directory_iterator(), sets);
+        CHECK(plugins == std::vector{FilePath(out, u8"dummy_sse", u8"", u8".esp", FileTypes::Plugin)});
+        clean_dummy_plugins(plugins, sets);
+        CHECK(plugins.empty());
+        CHECK(btu::common::compare_directories(out, dir / "expected"));
+    }
+
+     SECTION("empty directory")
+    {
+        const auto dir = TempPath(btu::fs::temp_directory_path());
+        btu::fs::create_directories(dir.path());
+
+        auto plugins = list_plugins(btu::fs::directory_iterator(dir.path()), btu::fs::directory_iterator(), sets);
+        CHECK(plugins.empty());
+        clean_dummy_plugins(plugins, sets);
+        CHECK(plugins.empty());
+        CHECK(is_empty(dir.path()));
+    }
+}
+
+TEST_CASE("find_archive_name with plugins", "[src]")
+{
+    auto sets = Settings::get(btu::Game::SSE);
+
+    SECTION("handles empty plugin list")
+    {
+        std::span<const FilePath> plugins;
+        auto result = find_archive_name(plugins, sets, ArchiveType::Textures);
+        CHECK_FALSE(result.has_value());
+    }
+
+    SECTION("returns name with incremented counter if an archive is already linked to a plugin")
+    {
+        const auto dir = TempPath(btu::fs::temp_directory_path());
+        btu::fs::create_directories(dir.path());
+
+        create_file(dir.path() / "ExistingPlugin.esp");
+        create_file(dir.path() / "ExistingPlugin.bsa");
+        create_file(dir.path() / "ExistingPlugin - Textures.bsa");
+
+        std::vector plugins = {
+            FilePath::make(dir.path() / "ExistingPlugin.esp", sets, FileTypes::Plugin).value(),
+        };
+
+        auto result = find_archive_name(plugins, sets, ArchiveType::Standard);
+        REQUIRE(result.has_value());
+        INFO(btu::common::as_ascii(result.value().full_path().u8string()));
+        CHECK(result->name == u8"ExistingPlugin");
+        CHECK(result->counter == 0);
+    }
+}
+
+TEST_CASE("find_archive_name tests with directory input", "[src]")
+{
+    auto sets = Settings::get(btu::Game::SSE);
+
+    SECTION("handles empty directory")
+    {
+        const Path dir = "empty_directory";
+        auto result = find_archive_name(dir, sets, ArchiveType::Textures);
+        CHECK_FALSE(result.has_value());
+    }
+
+    SECTION("finds a name if a plugin exists")
+    {
+        const Path dir = "bsa_clean_dummy_plugins/in";
+
+        auto result = find_archive_name(dir, sets, ArchiveType::Textures);
+        CHECK(result.has_value());
+        CHECK(result->name == u8"dummy_sse"sv);
+    }
+
+    SECTION("returns nullopt if all possible file names in directory are taken")
+    {
+        const Path dir = "directory_with_all_possible_names_taken";
+        auto result = find_archive_name(dir, sets, ArchiveType::Textures);
+        CHECK_FALSE(result.has_value());
+    }
 }
